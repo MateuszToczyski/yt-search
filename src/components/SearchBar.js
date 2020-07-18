1fa49fc9e82a24840f819d9f02c514f5799d76ab
Mateusz Toczyski <toczyski.mateusz@gmail.com>
2020-07-18 13:34:12 +0200

    Update label

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,7 +13,7 @@ export default ({ onFormSubmit }) => {
     <div className="search-bar ui segment">
       <form onSubmit={onSubmit} className="ui form">
         <div className="field">
-          <label>YouTube Video Search</label>
+          <label>Video Search</label>
           <input
             type="text"
             value={term}
